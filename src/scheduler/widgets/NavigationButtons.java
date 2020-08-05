c5439da3117dd94db24bb925b3902db9b4996b39
itsakademiks <itsakademiks@users.noreply.github.com>
2020-08-05 15:55:47 -0400

    Update NavigationButtons.java
    Changed Navigation Bar, Restyled the nav bar, Changed layout

--- src/scheduler/widgets/NavigationButtons.java
+++ src/scheduler/widgets/NavigationButtons.java
@@ -1,51 +1,49 @@
 package scheduler.widgets;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.VBox;
 import scheduler.models.Controller;
 
+import java.util.ArrayList;
+
 public class NavigationButtons extends VBox {
 
-    Button dashBoardButton = new Button("My Dashboard");
-    Button myScheduleButton = new Button("My Schedule");
-    Button createGroupButton = new Button("Create Group");
-    Button ViewGroupsButton = new Button("View Groups");
-    Button ViewMapButton = new Button("View Map");
-    Button optionsButton = new Button("Options");
-    Button logoutButton = new Button("Log out");
+    Label dashBoardButton   = new Label("My Dashboard");
+    Label myScheduleButton  = new Label("My Schedule");
+    Label createGroupButton = new Label("Create Group");
+    Label ViewGroupsButton  = new Label("View Groups");
+    Label ViewMapButton     = new Label("View Map");
+    Label optionsButton     = new Label("Options");
+    Label logoutButton      = new Label("Log out");
 
     public NavigationButtons() {
 
-        dashBoardButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("dashboard");
-        });
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(dashBoardButton);
+        labels.add(myScheduleButton);
+        labels.add(createGroupButton);
+        labels.add(ViewGroupsButton);
 
-        myScheduleButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("schedule");
-        });
+        labels.add(ViewMapButton);
+        labels.add(optionsButton);
 
-        createGroupButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("create");
-        });
+        setUpMenu(labels, "dashboard","schedule","create","view", "map","options");
 
-        ViewGroupsButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("view");
-        });
-        
-        ViewMapButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("map");
-        });
+        setStyle("-fx-background-color: cyan");
+        setPadding(new Insets(5,2,5,2));
+        setMinWidth(200);
 
-        optionsButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().goToScene("options");
-        });
 
-        logoutButton.setOnAction((ActionEvent e) -> {
-            Controller.getInstance().logout();
-        });
-        VBox main = new VBox(5);
-        main.getChildren().addAll(
+
+        logoutButton.setOnMouseClicked(mouseEvent-> Controller.getInstance().logout());
+        logoutButton.setMinHeight(40);
+        logoutButton.setStyle("-fx-background-color: yellow; -fx-font-size: 16; -fx-cursor: hand");
+        logoutButton.setMinWidth(200);
+
+        setSpacing(5);
+        getChildren().addAll(
                 dashBoardButton,
                 myScheduleButton,
                 createGroupButton,
@@ -54,10 +52,16 @@ public class NavigationButtons extends VBox {
                 optionsButton,
                 logoutButton
         );
+    }
 
-        this.getChildren().addAll(
-                main
-        );
+    private void setUpMenu(ArrayList<Label> labels, String... scenes){
 
+        for (int i = 0; i < labels.size(); i++) {
+            int finalI = i;
+            labels.get(finalI).setOnMouseClicked(mouseEvent -> Controller.getInstance().goToScene(scenes[finalI]));
+            labels.get(finalI).setMinHeight(40);
+            labels.get(finalI).setStyle("-fx-background-color: yellow; -fx-font-size: 16; -fx-cursor: hand");
+            labels.get(finalI).setMinWidth(200);
+        }
     }
 }
