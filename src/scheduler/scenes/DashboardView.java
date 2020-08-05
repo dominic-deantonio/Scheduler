cbd6d2cc7f3ee2294dc95327bfcd021d86df49ee
itsakademiks <itsakademiks@users.noreply.github.com>
2020-08-05 15:59:01 -0400

    Update DashboardView.java
    Nav bar style update

--- src/scheduler/scenes/DashboardView.java
+++ src/scheduler/scenes/DashboardView.java
@@ -1,41 +1,40 @@
 package scheduler.scenes;
 
+import scheduler.utilities.Constants;
 import scheduler.widgets.CalendarWeekGui.CalendarWeek;
 import scheduler.widgets.*;
 import javafx.scene.layout.*;
-import javafx.geometry.Insets;
 import scheduler.models.Meeting;
 import scheduler.models.User;
 import scheduler.widgets.CalendarWeekGui.MeetingDetail;
 
 public class DashboardView extends BorderPane {
 
-    Insets insets = new Insets(15);
 
     public DashboardView(User user) {
         super();
-
         MeetingDetail meetingDetail = new MeetingDetail();
+        setStyle("-fx-background-color: #555");
 
         //Create the center node and children
         VBox center = new VBox();
         center.getChildren().add(new CalendarWeek(Meeting.getMockAppointments(), meetingDetail));
 
         //Create the left node and children
-        VBox left = new VBox();
-        left.getChildren().add(new NavigationButtons());
+        NavigationButtons left = new NavigationButtons();
 
         VBox right = new VBox();
         right.getChildren().addAll(new CalendarMonth(), meetingDetail);
 
         //Set the attributes of this scene
-        DashboardView.setMargin(left, insets);
+        DashboardView.setMargin(left, Constants.MENU_INSETS);
 
         //Add the nodes to this object
         this.setTop(new SceneHeader("Dashboard", user));
         this.setCenter(center);
         this.setLeft(left);
         this.setRight(right);
+
 
     }
 }
