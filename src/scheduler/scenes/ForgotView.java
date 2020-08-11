990a53bd37b75c1d79c18e75cc12668847546ce7
jennyroot <47508509+jennyroot@users.noreply.github.com>
2020-08-10 20:59:32 -0400

    Forgot Password Updates
    Modified scene to tell user to contact Super Scheduler support to reset their password.

--- src/scheduler/scenes/ForgotView.java
+++ src/scheduler/scenes/ForgotView.java
@@ -10,23 +10,10 @@ import scheduler.models.Controller;
 
 public class ForgotView extends VBox {
 
-    TextField emailTextField = new TextField();
-    Button submitButton = new Button("Submit");
-    Button goLoginButton = new Button("Log in instead");
-
+    Button goLoginButton = new Button("Return to Login Screen");
+    
     public ForgotView() {
         super(5);
-
-        //Email field
-        emailTextField.setPromptText("Email");
-        emailTextField.setPrefWidth(200);
-
-        //Submit button
-        submitButton.setStyle(Constants.MAIN_THEME_COLOR);
-        submitButton.setPrefWidth(200);
-        submitButton.setOnAction((ActionEvent e) -> {
-            System.out.println("Submit request for forgotton password");
-        });
 
         //Back to login button
         goLoginButton.setPrefWidth(200);
@@ -38,12 +25,8 @@ public class ForgotView extends VBox {
         this.setFillWidth(false);
         this.setAlignment(Pos.CENTER);
         this.getChildren().addAll(
-                new Text("Enter your email below and we'll send you a link to reset your password.\n"),
-                emailTextField,
-                new Text("\n"),
-                submitButton,
-                goLoginButton,
-                new Text("\n")
+                new Text("Please contact Super Scheduler support to change your password.\n"),
+                goLoginButton
         );
     }
 
